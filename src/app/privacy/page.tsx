f0bfcd5386361ba0facd1248532a702e330b1882
Rusty Tanton <rusty.tanton@gmail.com>
2024-06-09 19:41:47 -0400

    add stub privacy and tos pages

---
+++ src/app/privacy/page.tsx
@@ -0,0 +1,7 @@
+'use server';
+
+export default async function TermsPage() {
+    return (
+        <div>This will be a terms page. Adding a stub so the Google OAuth link doesn&lsquo;t 404.</div>
+    )
+}
---
+++ src/app/tos/page.tsx
@@ -0,0 +1,7 @@
+'use server';
+
+export default async function PrivacyPage() {
+    return (
+        <div>This will be a privacy page. Adding a stub so the Google OAuth link doesn&lsquo;t 404.</div>
+    )
+}
