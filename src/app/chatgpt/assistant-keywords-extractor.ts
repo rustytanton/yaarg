5e632289603e2a305d1d52c571e3953980d9967a
Rusty Tanton <rusty.tanton@gmail.com>
2024-07-02 19:00:13 -0400

    fix type error

--- src/app/chatgpt/assistant-keywords-extractor.ts
+++ src/app/chatgpt/assistant-keywords-extractor.ts
@@ -1,3 +1,4 @@
+import { MessageContent } from "openai/resources/beta/threads/messages.mjs";
 import { assistant, assistantProperties, assistantMessage } from "./assistant";
 
 export const assistantKeywordExtractor: assistantProperties = {
@@ -16,13 +17,19 @@ Return results in a JSON object which looks like this:
     model: 'gpt-3.5-turbo'
 }
 
+type KeywordsMessageContent = MessageContent & {
+    text: {
+        value: string
+    }
+}
+
 export async function getKeywords(prompt: string) {
     const _assistant = await assistant(assistantKeywordExtractor)
     if (_assistant) {
         const messages = await assistantMessage(_assistant.externalId, prompt)
         if (messages) {
-            const result = messages.data[0].content[0].text
-            return JSON.parse(result.value)
+            const result = messages.data[0].content[0] as KeywordsMessageContent
+            return JSON.parse(result.text.value)
         }
     }
 }
