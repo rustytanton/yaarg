29f415c1a6e3045c304c6c5ce7d2705f4be248ae
Rusty Tanton <rusty.tanton@gmail.com>
2024-07-03 15:11:11 -0400

    remove unused FormSubmitButton.tsx component

--- src/app/components/FormSubmitButton.tsx
+++
@@ -1,5 +0,0 @@
-export default function FormSubmitButton({ buttonText = 'Submit' }) {
-    return (
-        <input className="m-2 p-1 bg-gray-200 text-black" type="submit" value={buttonText} />
-    )
-}
