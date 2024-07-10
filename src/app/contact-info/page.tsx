827e30f34d0e7dbc71ca554f89cd40b86e9fa78e
Rusty Tanton <rusty.tanton@gmail.com>
2024-07-10 11:43:56 -0400

    fix typo

--- src/app/contact-info/page.tsx
+++ src/app/contact-info/page.tsx
@@ -5,7 +5,7 @@ import { auth } from '../auth';
 import ContactInfoForm from './form';
 import NoAccessMessage from '@/app/_lib/components/NoAccessMessage';
 
-export default async function TestPage() {
+export default async function ContactInfoPage() {
     const session = await auth()
     if (session?.user) {
         const user = await getUser(session.user.id as string)
