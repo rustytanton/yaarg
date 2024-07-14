2bcadb50fa6bf7f317a81503534db5cbfcd2f973
Rusty Tanton <rusty.tanton@gmail.com>
2024-07-14 19:58:40 -0400

    misc UI fixes

--- src/app/_lib/components/print-resume/ResumePrintHeader.tsx
+++ src/app/_lib/components/print-resume/ResumePrintHeader.tsx
@@ -7,10 +7,14 @@ type Props = {
 export default function ResumePrintHeader({ user }: Props) {
     return (
         <header className="mb-10">
-            <div className="flex">
+            <div>
                 <div className="pr-10">
                     <h1 className="text-4xl mb-2">{user?.firstName} {user?.lastName}</h1>
-                    <div className="mb-2">{user?.location} | {user?.phoneNumber} | {user?.email}</div>
+                    <div className="mb-2 divide-solid divide-x-2 divide-black">
+                        {user?.location ? <span className="pr-2 pl-2 first:pl-0">{user?.location}</span> : '' }                        
+                        {user?.phoneNumber ? <span className="pr-2 pl-2 first:pl-0">{user?.phoneNumber}</span> : ''}
+                        {user?.email ? <span className="pr-2 pl-2 first:pl-0">{user?.email}</span> : ''}
+                    </div>
                 </div>
                 <div>
                     <div>{user?.github}</div>
