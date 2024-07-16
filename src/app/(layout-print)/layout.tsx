cd7735d043406b85efb512f253ad6bbe48296417
Rusty Tanton <rusty.tanton@gmail.com>
2024-07-15 23:31:40 -0400

    Revert "add speed insights"
    This reverts commit c81e8b850b7f2232786482c3ef1ed881c3f37ad2.

--- src/app/(layout-print)/layout.tsx
+++ src/app/(layout-print)/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next"
 import { Inter } from "next/font/google"
 import "./globals.css";
-import { SpeedInsights } from "@vercel/speed-insights/next"
+
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,7 +20,6 @@ export default async function RootLayout({
       <body className={inter.className + " bg-white text-black"}>
           {children}
       </body>
-      <SpeedInsights />
     </html>
   );
 }
