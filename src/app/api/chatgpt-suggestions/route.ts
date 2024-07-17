d850dce6c745288ab9b005a2c947d77f415be3c3
Rusty Tanton <rusty.tanton@gmail.com>
2024-07-17 14:33:18 -0400

    (more) testing serverless function

--- src/app/api/chatgpt-suggestions/route.ts
+++ src/app/api/chatgpt-suggestions/route.ts
@@ -4,12 +4,13 @@ import prisma from "@/app/db"
 
 export const runtime = 'nodejs'
 
-export function GET() {
+export async function GET() {
 
     // testing if this will work with a serverless function
-    const resume = prisma.resume.findFirst()
+    const resume = await prisma.resume.findFirst()
 
     return Response.json({
-        jobDescription: resume.jobDescription
+        employer: resume?.employer,
+        testField: 'testing'
     })
 }
