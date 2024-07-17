8722db315353a95b1fdb4a6ab925ce8ba8d34f14
Rusty Tanton <rusty.tanton@gmail.com>
2024-07-17 14:29:19 -0400

    testing serverless function

--- src/app/api/chatgpt-suggestions/route.ts
+++ src/app/api/chatgpt-suggestions/route.ts
@@ -1,9 +1,15 @@
 // import { waitUntil } from '@vercel/functions'
 
+import prisma from "@/app/db"
+
 export const runtime = 'nodejs'
 
 export function GET() {
-    return new Response(`I am a Serverless Function`, {
-        status: 200,
+
+    // testing if this will work with a serverless function
+    const resume = prisma.resume.findFirst()
+
+    return Response.json({
+        jobDescription: resume.jobDescription
     })
 }
