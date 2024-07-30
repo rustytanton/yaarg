dbda02e86e1542f3287f33b20b5b020384ecc5af
Rusty Tanton <rusty.tanton@gmail.com>
2024-07-29 22:54:50 -0400

    adjustment to make resume redraw less pronounced

--- src/app/_lib/components/resume/ResumeContainer.tsx
+++ src/app/_lib/components/resume/ResumeContainer.tsx
@@ -4,6 +4,6 @@ type Props = {
 
 export default function ResumeContainer({ children }: Props) {
     return (
-        <div className="bg-white text-black p-10 mb-5 w-full">{children}</div>
+        <div className="bg-white text-black p-10 mb-5">{children}</div>
     )
 }
