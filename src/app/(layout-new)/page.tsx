e0c8afffeeb8e3506b10f2407f1d9868f458865b
Rusty Tanton <rusty.tanton@gmail.com>
2024-07-30 13:55:46 -0400

    add loom walkthrough to logged out state homepage

--- src/app/(layout-new)/page.tsx
+++ src/app/(layout-new)/page.tsx
@@ -46,7 +46,15 @@ export default async function Home() {
       <div className="flex justify-center">
         <div className="md:w-1/4">
           <p className="pb-5">Welcome! After you sign in with a valid email address, you can use this tool to build resumes for jobs.</p>
-          <p className="pb-5">As you create more resumes, the tool will use experience and other information you&apos;ve entered into past resumes to suggest experience bullet points customized for the job you&apos;re applying for.</p>
+          <p className="pb-5 mb-10">As you create more resumes, the tool will use experience and other information you&apos;ve entered into past resumes to suggest experience bullet points customized for the job you&apos;re applying for.</p>
+          <div className="LoomEmbed">
+            <iframe
+              src="https://www.loom.com/embed/ed4a854ec2c7408dae87f993b57b9779?sid=04fefc13-cb39-4fc0-931f-397fb1355057"
+              frameBorder="0"
+              allowFullScreen
+            >
+            </iframe>
+          </div>
         </div>
       </div>
     )
